44870b853baa8579bc894d1e9e282689208994bf
Amin_117 <garridoamin@gmail.com>
2025-11-14 10:16:02 -0300

    pagina de tecnicas de elicitacion terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-export const App = () => {
-  return <div>App</div>;
-};
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TecnicasDeElicitacion from "./pages/TecnicasDeElicitacion";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tecnicas" element={<TecnicasDeElicitacion />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
