1260b8d44dc6301475936745c6f7e5c6559621b8
Maximo Veron <maximoveron49@gmail.com>
2025-11-14 09:58:10 -0300

    estructuracion de las secciones de la pagina en base al nav

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+export const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Sobre la ingenieria de software</li>
+          <li>conceptos fundamentales</li>
+          <li>Tecnicas de elicitacion</li>
+          <li>Proceso de Elicitacion</li>
+          <li>StakeHolders</li>
+          <li>Herramientas y recursos</li>
+          <li>Documentacion de requerimientos</li>
+          <li>Validacion y Priorizacion</li>
+          <li>Ejemplos Practicos</li>
+          <li>juegos</li>
+          <li>Glosario</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
